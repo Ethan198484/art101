c5ff1a6edb1fb6235af08a583d7cf2be066f659b
Ethan198484 <Eprice112004@gmail.com>
2025-04-21 21:11:42 -0700

    Lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,42 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Four in a Row
+    // Ethan Price <elprice@ucsc.edu>
+    // April 2024
+
+
+
+		// Print the board before each turn
+				// Loop through each row
+					// Print X, 0, or Space
+					// Print a line between
+				// Print a newline
+				// Print a horizontal line after each row
+
+		// Game Loop
+		//Loop until board is full OR four in a row:
+				// Accept user input
+				// Prompt user for column choice
+
+				// Validate user move 
+				// While chosen column is full:
+					// Print "Choose another column"
+					// Prompt user for new column choice
+
+				// Place tile
+				// Locate topmost empty row in chosen column
+				// Place current player's tile (X/0)
+
+				// Check for win
+				// If four tiles in a row (horizontal/vertical/diagonal):
+					// Print "Player {tile(X/0)} wins!"
+					// Break loop/Return
+
+				// Check for tile
+				// If all tiles filled
+					// Print ("TIe Game")
+					// Break loop/Return
+
+					//  Switch players
+					// Alternate current player (from X to 0 or 0 to X)
 
 // Constants
 
