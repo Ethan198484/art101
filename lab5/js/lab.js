8a7f39370aad3d30ff33af7da4157ad795ebda78
Ethan198484 <Eprice112004@gmail.com>
2025-04-24 19:07:22 -0700

    Lab 5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,10 +1,34 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 5 java interactiion 
+// Author: Ethan Price
+// Date: 24 April
 
 // Constants
 
-// Functions
+// Define Variables
+
+var make = "Ford";
+
+var model = "F-150";
+
+var color = "Green";
+
+var year = "1997";
+
+let ownIt = true;
+
+// calculate 
+
+let age = 2025-year;
+
+
+
+// output
+document.writeln("make: " + make +"<br>");
+document.writeln("model: " + model +"<br>");
+document.writeln("color: " + color +"<br>");
+document.writeln("age: " + age +"years<br>");
+
+
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
