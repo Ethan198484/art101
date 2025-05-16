ac28b98daa80b2346fa0a43064eea88de62fbe85
Ethan198484 <Eprice112004@gmail.com>
2025-05-15 21:03:23 -0700

    Lab 10

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -1,31 +1,13 @@
-
-
 // index.js - Lab 9 Libraries and JQuery
 // Author: Ethan Price
-// Date: 12 May 2025
-
-//add button to challenge section
-$("#challenge").append("<button id='button-challenge'>Make Special</button>");
-
-//add click listener
-$("#button-challenge").click(function() {
-    $("#challenge").toggleClass("special");
-});
-
-
-$("#problems").append("<button id='button-problems'>Make Special</button>");
-
-//add click listener
-$("#button-problems").click(function() {
-    $("#problems").toggleClass("special");
-});
+// Date: 12 May
 
 
 
-$("#results").append("<button id='button-results'>Make Special</button>");
 
-//add click listener
-$("#button-results").click(function() {
-    $("#results").toggleClass("special");
-});
+$(".special-sections").append("<button>Make Special!</button>");
 
+$(".special-sections button").click(function() {
+   console.log("button is clicked")
+   $(this).parent().toggleClass("special")
+}); 
