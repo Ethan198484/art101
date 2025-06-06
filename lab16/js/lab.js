ae699bc217da40c96812f504a32be4f917087a8b
Ethan198484 <Eprice112004@gmail.com>
2025-06-05 18:49:21 -0700

    lab16

---
+++ lab16/js/lab.js
@@ -0,0 +1,13 @@
+// index.js - Lab 16 JSON & API's
+// Author: Ethan Price
+// Date: 5 June
+
+
+
+
+var comicObj = {
+  "title": "My Comic",
+  "img": "img/lilguy.jpg",
+  "alt": "An example comic"
+}
+
